744b6531861e21105719c4d41c1109388321e0b5
Eugene <jekkogm@gmail.com>
2025-03-13 16:51:21 +0200

    Fixed phpstan errors

--- database/migrations/2024_12_17_000001_create_jwt_refresh_tokens_table.php
+++ database/migrations/2024_12_17_000001_create_jwt_refresh_tokens_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('jwt_refresh_tokens', function (Blueprint $table) {
-            $table->char('jti', 26)->primary(false);
+            $table->char('jti', 26)->primary();
             $table->char('access_token_jti', 26)->index();
             $table->string('subject', 36);
             $table->timestamp('expired_at');
