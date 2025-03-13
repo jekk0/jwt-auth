54bf4e96ddde984e37b9cb3f6065997d35025def
Eugene <jekkogm@gmail.com>
2025-03-13 16:53:49 +0200

    Fixed phpstan errors

--- src/JwtAuthServiceProvider.php
+++ src/JwtAuthServiceProvider.php
@@ -35,7 +35,7 @@ final class JwtAuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
-            /** @phpstan-ignore-line */
+            /** @phpstan-ignore-next-line */
             $publishesMigrationsMethod = method_exists($this, 'publishesMigrations')
                 ? 'publishesMigrations' : 'publishes';
 
